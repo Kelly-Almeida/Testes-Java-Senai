949f0ecaf50d86d8fd6151650c427a8bb9934774
Kelly Almeida <kellydealmeidasantos237@gmail.com>
2026-03-11 15:28:12 -0300

    Primeira versão funcional

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,18 @@ void main() {
     Scanner scan = new Scanner(System.in);
     Boolean calc = true;
     //Calculadora simples
-    while (calc == true){
+    do{
+        Boolean fimEsc = false;
         int op = 0;
+        float resul;
 
-        System.out.println("======= Calcular ======");
+        System.out.println("\n======= Calcular ======");
+        System.out.println("=======================");
 
         //Escolha de primeiro número
-        System.out.println("Digite o primeiro número: ");
-        int n1 = scan.nextInt();
+        System.out.print("Digite o primeiro número: ");
+        float n1 = scan.nextFloat();
 
-        Boolean fimEsc = false;
         //Escolha da operação
         do{
             System.out.println("Escolha a operação: ");
@@ -31,6 +33,57 @@ void main() {
                 System.out.println("Escolha uma operação válida!!!");
             }
 
-        }while(fimEsc == false);
-    };
+        }while(!fimEsc);
+
+        fimEsc = false;
+        //Escolha do segundo número
+        float n2;
+        do{
+            System.out.print("Digite o segundo número: ");
+            n2 = scan.nextFloat();
+
+            //Teste de equação válida
+            if ((n2 == 0) && ((op == 5) || (op == 4))){
+                System.out.println("A equação é inválida!!!");
+            }else {
+                fimEsc = true;
+            }
+        }while(!fimEsc);
+
+        //Saída dos calculos
+        switch (op){
+            case 1:
+                resul = n1 + n2;
+                System.out.println(n1 + " + " + n2 + " = " + resul);
+                break;
+            case 2:
+                resul = n1 - n2;
+                System.out.println(n1 + " - " + n2 + " = " + resul);
+                break;
+            case 3:
+                resul = n1 * n2;
+                System.out.println(n1 + " * " + n2 + " = " + resul);
+                break;
+            case 4:
+                resul = n1 / n2;
+                System.out.println(n1 + " / " + n2 + " = " + resul);
+                break;
+            case 5:
+                resul = n1 % n2;
+                System.out.println(n1 + " % " + n2 + " = " + resul);
+                break;
+        }
+
+        //Escolha se vai desligar a calculadora
+        int seguir;
+        do{
+            System.out.println("Deseja continuar? \n[1] - Sim\n[2] - Não");
+            seguir = scan.nextInt();
+            if (seguir == 2){
+                calc = false;
+            } else if ((seguir != 1) && (seguir != 2)) {
+                System.out.println("Digite uma opção válida!!");
+            }
+        }while((seguir != 2) && (seguir != 1));
+    }while (calc);
 }
