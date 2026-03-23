c48ffb2a20d44ef705c7692ff1a1a0b38d4312c5
Kelly Almeida <kellydealmeidasantos237@gmail.com>
2026-03-23 11:45:38 -0300

    Create java5-2.java

---
+++ java5-2.java
@@ -0,0 +1,13 @@
+public class aula5e01 {
+    public static void main(String[] args) {
+        String[] cores = new String[]{"Azul", "Vermelho", "Preto", "Laranja", "Verde"};
+        System.out.println("A primeira cor é " + cores[0]);
+        System.out.println("A última cor é " + cores[4]);
+        System.out.println("\n--- Lista de cores ---\n");
+
+        for(int i = 0; i < cores.length; ++i) {
+            System.out.println("*" + cores[i] + ", posição " + i);
+        }
+
+    }
+}
