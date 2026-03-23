1e64360ef3b5010aba4257fd23e3f50f6b126064
Kelly Almeida <kellydealmeidasantos237@gmail.com>
2026-03-23 11:44:43 -0300

    Create aula5-2.java

---
+++ aula5-2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class aula5 {
+    public static void main (String[] args){
+        Scanner scan = new Scanner(System.in);
+        int[] nums = new int[10];
+        for(int i = 0; i < 10; i++){
+            System.out.println("Digite o "+ (i + 1) + "º número: ");
+            nums[i] = scan.nextInt();
+
+        }
+
+        for(int i = 0; i < 10; i++){
+            System.out.println((i + 1) + "° número: "+ nums[i]);
+        }
+    }
+}
