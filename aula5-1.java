09aa158810b898764e743f159a3098baa16f2036
Kelly Almeida <kellydealmeidasantos237@gmail.com>
2026-03-23 11:43:35 -0300

    Create aula5-1.java

---
+++ aula5-1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.util.Locale;
+public class Corpo{
+    public void main() {
+        Scanner scan = new Scanner(System.in);
+        String [] nomes = new String[5];
+
+        for (int i = 0; i < 5; i++){
+            System.out.println("Digite o " + (i + 1) +"º nome: ");
+            nomes[i] = scan.nextLine();
+        }
+
+        for(int i = 0; i < 5; i++){
+            System.out.println("Demônio "+ (i + 1) + "°: " + nomes[i]);
+        }
+    }
+}
+
