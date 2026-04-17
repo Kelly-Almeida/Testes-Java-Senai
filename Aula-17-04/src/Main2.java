e0e60d0d618a982319f19f68ed2a4966b4d7bbfe
kelly.asantos14 <kellydealmeidasantos237@gmail.com>
2026-04-17 18:31:09 -0300

    Parte da ação dos botões

--- Aula-17-04/src/Main2.java
+++ Aula-17-04/src/Main2.java
@@ -7,7 +7,7 @@ import java.io.*;
 public class Main2 extends JFrame {
     private JTextField txtFilme, txtGenero, txtValor;
     private JRadioButton rbTxt, rbCsv;
-    private JTextArea areaexibicao;
+    private JTextArea areaExibicao;
 
     public  Main2() {
         //Configuração básico de Janela
@@ -23,6 +23,63 @@ public class Main2 extends JFrame {
 
         painelFormulario.add(new JLabel("Nome do Filme"));
         txtFilme = new JTextField();
+        painelFormulario.add(txtFilme);
+
+        painelFormulario.add(new JLabel("Gênero"));
+        txtGenero = new JTextField();
+        painelFormulario.add(txtGenero);
+
+        painelFormulario.add(new JLabel("Valor da Locação (R$): "));
+        txtValor = new JTextField();
+        painelFormulario.add(txtValor);
+
+        //Painel de opção do formato do formulário
+        painelFormulario.add(new JLabel("Salvar com: "));
+        JPanel painelRadios = new JPanel(new FlowLayout(FlowLayout.LEFT));
+
+        rbTxt = new JRadioButton(".TXT (texto)");
+        rbCsv = new JRadioButton(".CSV (texto plus");
+        rbTxt.setSelected(true);
+
+        //Agrupa os botões para que apenas um seja selecionado por vez
+        ButtonGroup groupoArquivo = new ButtonGroup();
+        groupoArquivo.add(rbTxt);
+        groupoArquivo.add(rbCsv);
+
+        painelRadios.add(rbTxt);
+        painelRadios.add(rbCsv);
+        painelFormulario.add(painelRadios);
+
+        add(painelFormulario, BorderLayout.NORTH);
+
+        //Área central onde será exibido os dados
+
+        areaExibicao = new JTextArea();
+        areaExibicao.setEditable(false);
+        JScrollPane scrollPane = new JScrollPane(areaExibicao);
+        scrollPane.setBorder(BorderFactory.createTitledBorder("Registro Salvos: "));
+        add(scrollPane, BorderLayout.CENTER);
+
+        //Painel inferior, botões de ação
+        JPanel painelBotoes = new JPanel();
+        JButton btnSalvar = new JButton("Salvar Filme");
+        JButton btnLer = new JButton("Ler Arquivo");
+
+        painelBotoes.add(btnSalvar);
+        painelBotoes.add(btnLer);
+        add(painelBotoes, BorderLayout.SOUTH);
+
+        //Ação do botão Salvar
+
+        btnSalvar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {salvarDados();}
+        });
+
+        btnLer.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {lerdados();}
+        });
 
 
     }
