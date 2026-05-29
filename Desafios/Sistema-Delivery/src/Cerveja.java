f2dcb4f01ef843574dd2564b574a9902116ddf2c
KELLY DE ALMEIDA SANTOS <kelly.asantos14@senacsp.edu.br>
2026-05-29 18:56:56 -0300

    Começando a modelagem da classe Cerveja

--- Desafios/Sistema-Delivery/src/Cerveja.java
+++ Desafios/Sistema-Delivery/src/Cerveja.java
@@ -1,2 +1,16 @@
 public class Cerveja extends Produto{
+    private String nome;
+    private float valor;
+    private String marca;
+
+    public Cerveja (String marca){
+        this.marca = marca;
+        this.nome = "Cerveja " + getMarca();
+        if (marca.equalsIgnoreCase("Brahma")) this.valor = 12;
+        else if (marca.equalsIgnoreCase(""))
+    }
+
+    public String getMarca(){
+        return this.marca;
+    }
 }
