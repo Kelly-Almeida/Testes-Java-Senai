2e683c06d575512d5fa616372625f7e9c3806b38
Kelly Almeida <kellydealmeidasantos237@gmail.com>
2026-06-13 18:56:41 -0300

    Fix array initialization and method calls in Arena

--- POO/Polimorfismo/src/Arena.java
+++ POO/Polimorfismo/src/Arena.java
@@ -1,32 +1,27 @@
 public class Arena {
     public static void main(String[] args){
-        Personagem[] herois = new Personagem[0] {
-            new Guerreiro("Conan");
-            new Mago("Gandalf");
-            new Arqueiro("Legolas");
-
-            Personagem monstro = new Personagem("Dragão", 200, 0){
-                @Override
-                public void usarHabilidad(Personagem alvo){
-                    System.out.println("Dragão não tem  habilidade especiais");
-                }
-            };
-
-            System.out.println("======= A BATALHA VAI COMEÇAR! ========");
-            monstro.receberDano(0);
-            System.out.println("");
-
-            for (Personagem heroi : herois){
-                if ((monstro.estaVivo)){
-
-
-
-
-                    heroi.usarHabilidade(monstro);
-                    System.out.println("-----------------------------------------");
-                }
+        Personagem[] herois = new Personagem[] {
+            new Guerreiro("Conan"),
+            new Mago("Gandalf"),
+            new Arqueiro("Legolas")
+        };
+        Personagem monstro = new Personagem("Dragão", 200, 0){
+            @Override
+            public void usarHabilidade(Personagem alvo) {
+                System.out.println("Dagrão não tem habilidade especiais.");
             }
-            System.out.println("\n========== FIM DA BATALHA ==========");
+        };
+        System.out.println("======= A BATALHA VAI COMEÇAR! ========");
+        monstro.receberDano(0);
+        System.out.println("");
+
+        for (Personagem heroi : herois){
+            if ((monstro.estaVivo())){
+                heroi.usarHabilidade(monstro);
+                System.out.println("-----------------------------------------");
+            }
         }
+        System.out.println("\n========== FIM DA BATALHA ==========");
     }
 }
+
